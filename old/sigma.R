79191255a6fc97c6faeb70eb8af53c94c2584a35
annagroot <164199377+annagroot@users.noreply.github.com>
2024-12-17 16:33:00 +0100

    Update sigma.R

--- old/sigma.R
+++ old/sigma.R
@@ -1,6 +1,7 @@
 rm(list = ls())
 
 # devtools::install_github("jjallaire/sigma")
+devtools::install_github("annagroot/mywidget")
 
 library(htmlwidgets)
 ##Test
@@ -10,10 +11,12 @@ library(htmlwidgets)
 ## End test
 
 
-devtools::create("mywidget")               # create package using devtools
-htmlwidgets::scaffoldWidget("mywidget")    # create widget scaffolding
-devtools::document()                       # roxygenize, so NAMESPACE is updated
-devtools::install()                        # install the package so we can try it
+# devtools::create("mywidget")               # create package using devtools
+# htmlwidgets::scaffoldWidget("mywidget")    # create widget scaffolding
+# devtools::document()                       # roxygenize, so NAMESPACE is updated
+# devtools::install()                        # install the package so we can try it
 
 library(mywidget)
 mywidget("Hello, genius!")                   # test the widget
+
+mywidget
